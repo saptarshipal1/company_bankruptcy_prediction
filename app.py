564573104dbdebc630c74438952e195433a8885b
Saptarshi Pal <saptarshi.pal1@gmail.com>
2023-08-07 11:27:38 +0530

    changes in app

--- app.py
+++ app.py
@@ -8,21 +8,29 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from model import preprocess_inputs, cbp_model
 
-
+app = Flask(__name__)
 
 df = pd.read_csv('company_bankruptcy_prediction.csv')
-
-X_train, X_test, y_train, y_test = preprocess_inputs(df)
-
 model = pickle.load(open("cbp_best_model.pkl","rb"))
 
-sample_data = X_test.sample()
+@app.route('/',methods=['GET','POST'])  
 
-print('The sample data is \n' + sample_data)
+def predict_datapoint():
+                if request.method=='GET':
+                    return render_template('index.html')
+                else:
+                    X_train, X_test, y_train, y_test = preprocess_inputs(df)
 
-model = pickle.load(open("cbp_best_model.pkl","rb"))
+                    sample_data = X_test.sample()
+                    
+                    model.fit(X_train,y_train)
 
-model.fit(X_train,y_train)
+                    return render_template('index.html',random_input_data=sample_data[0])
 
-model.predict(sample_data)
+                    results = model.predict(sample_data)
+                    
+                    return render_template('index.html',results=results[0])
 
+if __name__ == '__main__':
+    app.run(debug=True,host="0.0.0.0",port=9696)
+    
